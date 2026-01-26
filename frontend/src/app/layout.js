f7f03af49c1a9db3b020e5a96d3021c1f7eb9c71
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-01-26 15:06:39 +0530

    Initial Setup

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Whisk - AI Recipe Planner",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        <footer className="py-8 px-4 border-t">
+          <div className="max-w-6xl flex items-center justify-center mx-auto ">
+            <p className="text-stone-600 text-sm">Made by Shahil Kataria</p>
+          </div>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return <div>
+    <Button variant="destructive">Click me</Button>
+  </div>;
+};
+
+export default page;
